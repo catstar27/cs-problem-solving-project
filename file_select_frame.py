337dd8e8312fb7b7892106254a9231704538d676
eingerson3419 <catstar27@tutanota.com>
2023-12-04 13:29:28 -0500

    Added comments and class descriptions

--- file_select_frame.py
+++ file_select_frame.py
@@ -10,19 +10,23 @@ class FileSelectFrame(QFrame):
         super().__init__()
         self.layout = QHBoxLayout()
 
+        # Filepath label setup
         self.file_path_label = QTextEdit("Path to File")
         self.file_path_label.setMaximumHeight(50)
         self.layout.addWidget(self.file_path_label)
 
+        # Browse button setup
         self.file_select_button = QPushButton()
         self.file_select_button.setText("Browse...")
         self.file_select_button.clicked.connect(self.on_file_select_button_pressed)
         self.layout.addWidget(self.file_select_button)
 
+        # File dialog setup
         self.file_dialog = QFileDialog()
         self.filter = "Sound files (*.mp3 *.wav)"
 
         self.setLayout(self.layout)
 
     def on_file_select_button_pressed(self):
+        # Opens the dialog window and sets the text edit label to the filepath
         self.file_path_label.setText(self.file_dialog.getOpenFileName(self, "Open File", "", self.filter)[0])
