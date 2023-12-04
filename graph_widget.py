30c53219c063a328e94721b732ac7f61f96e9882
Elijah Ingerson <catstar27@tutanota.com>
2023-12-04 18:53:36 -0500

    Updated and fixed up graph widget, added function to plot a new function

--- graph_widget.py
+++ graph_widget.py
@@ -8,18 +8,19 @@ class GraphWidget(FigureCanvas):
     """
     def __init__(self, x, y):
         super().__init__(Figure())
-        subplot = self.figure.subplots()
-        subplot.plot(x, y, "k")
+        self.subplot = self.figure.subplots()
 
         # Color customization
         self.figure.set_facecolor((.26, .33, .52))
-        subplot.set_facecolor((.26, .33, .52))
-        subplot.spines['bottom'].set_color((.961, .910, .780))
-        subplot.spines['top'].set_color((.961, .910, .780))
-        subplot.spines['left'].set_color((.961, .910, .780))
-        subplot.spines['right'].set_color((.961, .910, .780))
-        subplot.xaxis.label.set_color((.961, .910, .780))
-        subplot.tick_params(axis='x', colors=(.961, .910, .780))
-        subplot.yaxis.label.set_color((.961, .910, .780))
-        subplot.tick_params(axis='y', colors=(.961, .910, .780))
+        self.subplot.set_facecolor((.26, .33, .52))
+        self.subplot.spines['bottom'].set_color((.961, .910, .780))
+        self.subplot.spines['top'].set_color((.961, .910, .780))
+        self.subplot.spines['left'].set_color((.961, .910, .780))
+        self.subplot.spines['right'].set_color((.961, .910, .780))
+        self.subplot.xaxis.label.set_color((.961, .910, .780))
+        self.subplot.tick_params(axis='x', colors=(.961, .910, .780))
+        self.subplot.yaxis.label.set_color((.961, .910, .780))
+        self.subplot.tick_params(axis='y', colors=(.961, .910, .780))
 
+    def new_plot(self, x, y):
+        self.subplot.plot(x, y, "k")
