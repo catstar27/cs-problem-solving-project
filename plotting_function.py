a2198891099b0250ecef3498b080d1a095aba929
Gale Falikson <afalikson1705@floridapoly.edu>
2023-12-04 19:35:23 -0500

    Updated the model code to process arrays more accuratly

--- plotting_function.py
+++ plotting_function.py
@@ -1,32 +1,59 @@
 import numpy as np
+import scipy.signal as signal
+import math
 
+
+def my_resample(x, newnum, y):
+    method = 0
+
+    if (0 == method):
+        if (len(y) != len(x)):
+            print("my_resample: Error: lengths of x and y are not equal!")
+        # pad signal such that its length is a power of 2 = much faster
+        orig_len = len(x)
+        p2_len = int(math.pow(2, math.ceil(math.log(orig_len) / math.log(2))))
+        x3 = np.zeros(p2_len)
+        y3 = np.zeros(p2_len)
+        x3[0:orig_len - 1] = x[0:orig_len - 1]
+        y3[0:orig_len - 1] = y[0:orig_len - 1]
+        x2, y2 = signal.resample(x3, newnum * p2_len // orig_len, y3)
+        x2 = x2[0:newnum - 1]
+        y2 = y2[0:newnum - 1]
+    else:
+        newnum = int(newnum)
+        num = len(x)
+        stride = int(num / newnum)
+        x2 = np.zeros(newnum)
+        y2 = np.zeros(newnum)
+        i = 0
+        for i2 in range(0, newnum):
+            i = i2 * stride
+            x2[i2] = x[i]
+            y2[i2] = y[i]
+    return x2, y2
 
 class RT60:
-    def measure(self, h, fs=1, decay_db=60, rt60_tgt=None):
-        h = np.array(h)
-        fs = float(fs)
+    def update_graph(self, amp, t):
+        """Updates the graph with new data/annotations"""
 
-        # The power of the impulse response in dB
-        power = h ** 2
-        energy = np.cumsum(power[::-1])[::-1]  # Integration according to Schroeder
+        if len(amp) <= 1: return  # do nothing when there's nothing to graph
 
-        # remove the possibly all zero tail
-        i_nz = np.max(np.where(energy > 0)[0])
-        energy = energy[:i_nz]
-        energy_db = 10 * np.log10(energy)
-        energy_db -= energy_db[0]
+        epsilon = 1.0e-8  # added to avoid log(0) errors
 
-        # -5 dB headroom
-        i_5db = np.min(np.where(-5 - energy_db > 0)[0])
-        e_5db = energy_db[i_5db]
-        t_5db = i_5db / fs
+        # Compute the quantity to be plotted
+        power = (amp + epsilon) ** 2
+        maxval = 1.0 * np.max(power)
+        power = power / maxval
+        dB = 10 * np.ma.log10(np.abs(power))  # "ma"=masked array, throws out -Inf values
 
-        # after decay
-        i_decay = np.min(np.where(-5 - decay_db - energy_db > 0)[0])
-        t_decay = i_decay / fs
+        # Downsample for plotting purposes.  (otherwise the plotting takes forever)
+        nsamples = len(dB)
+        if (nsamples > 100000):
+            plotsamples = 2048
+        elif (nsamples > 50000):
+            plotsamples = 2048
+        else:
+            plotsamples = 1024
+        ds_dB, ds_t = my_resample(dB, plotsamples, t)
 
-        # compute the decay time
-        decay_time = t_decay - t_5db
-        est_rt60 = (60 / decay_db) * decay_time
-
-        return est_rt60
+        return ds_dB, ds_t
