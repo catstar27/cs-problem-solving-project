5fd7cae883c7420a334703bc8a5eaf8713434137
Gale Falikson <afalikson1705@floridapoly.edu>
2023-12-06 18:03:08 -0500

    Added functions to allow for further RT60 calculations

--- plotting_function.py
+++ plotting_function.py
@@ -1,18 +1,27 @@
 import numpy as np
 
-class RT60():
-    def __init__(self, freqs, ranges):
-        self.freqs = freqs
+class RT60:
+    def __init__(self, ranges, spectrum, freqs, time, im):
         self.ranges = ranges
+        self.spectrum = spectrum
+        self.freqs = freqs
+        self.time = time
+        self.im = im
     def range_finder(self):
         for x in self.freqs:
             if x > self.ranges:
                 break
         return x
-    def frequency_check(self, spectrum):
-        target_frequency = self.freqs.range_finder(self.ranges)
+    def frequency_check(self):
+        target_frequency = self.freqs.range_finder()
         index_of_frequency = np.where(self.freqs == target_frequency)[0][0]
-        data_for_frequency = spectrum[index_of_frequency]
+        data_for_frequency = self.spectrum[index_of_frequency]
 
         data_in_db_fun = 10 * np.log10(data_for_frequency)
         return data_in_db_fun
+def find_nearest_value(array, value):
+    array = np.asarray(array)
+    idx = (np.abs(array - value)).argmin()
+    return array[idx]
+
+
