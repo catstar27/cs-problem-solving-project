673a2431c383c7e8468764a942a292ec6cca1f6d
Gale Falikson <afalikson1705@floridapoly.edu>
2023-12-06 16:29:55 -0500

    Removed un-needed self.variables

--- plotting_function.py
+++ plotting_function.py
@@ -1,10 +1,8 @@
 import numpy as np
 
 class RT60():
-    def __init__(self, freqs, range, spectrum):
+    def __init__(self, freqs):
         self.freqs = freqs
-        self.range = range
-        self.spectrum = spectrum
     def range_finder(self, range):
         for x in self.freqs:
             if x > range:
@@ -13,7 +11,7 @@ class RT60():
     def frequency_check(self, spectrum):
         target_frequency = self.freqs.range_finder(self.range)
         index_of_frequency = np.where(self.freqs == target_frequency)[0][0]
-        data_for_frequency = self.spectrum[index_of_frequency]
+        data_for_frequency = spectrum[index_of_frequency]
 
         data_in_db_fun = 10 * np.log10(data_for_frequency)
         return data_in_db_fun
