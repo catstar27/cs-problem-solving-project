a71a706636cdc95569269ec2a01820c3cd4e818b
Gale Falikson <afalikson1705@floridapoly.edu>
2023-12-06 16:41:02 -0500

    Fixed a few variables

--- plotting_function.py
+++ plotting_function.py
@@ -1,15 +1,16 @@
 import numpy as np
 
 class RT60():
-    def __init__(self, freqs):
+    def __init__(self, freqs, ranges):
         self.freqs = freqs
-    def range_finder(self, range):
+        self.ranges = ranges
+    def range_finder(self):
         for x in self.freqs:
-            if x > range:
+            if x > self.ranges:
                 break
         return x
     def frequency_check(self, spectrum):
-        target_frequency = self.freqs.range_finder(self.range)
+        target_frequency = self.freqs.range_finder(self.ranges)
         index_of_frequency = np.where(self.freqs == target_frequency)[0][0]
         data_for_frequency = spectrum[index_of_frequency]
 
