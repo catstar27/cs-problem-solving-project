e95dcc4ee6da1e68dd22aeeffa7aa90417355913
Lumi <catstar27@tutanota.com>
2023-12-07 21:00:37 -0500

    Separated controller into controller and view for organization

--- controller.py
+++ controller.py
@@ -1,14 +1,6 @@
-import sys
-from PyQt6.QtWidgets import *
-from PyQt6.QtCore import *
-import waveform_model as wm
-import file_select_frame as fsf
-import file_load_frame_view as flf
-import reverbform_model as rm
-import graph_model as gm
-import rt_alternate_buttons_view as rtab
 import atexit
 import os
+from view import View
 
 pi = 3.14159
 frequencies = ["Low", "Mid", "High", "All"]
@@ -16,36 +8,34 @@ frequencies = ["Low", "Mid", "High", "All"]
 
 
 def set_rt_graph(num):
-    global current_graph_index, loaded
-    if loaded:
-        current_graph_index += num
-        current_graph_index %= 4
-        if frequencies[current_graph_index] != "All":
-            rt_graph.plot_waveform(frequencies[current_graph_index])
+    if app.is_file_loaded:
+        app.current_rt_graph_index += num
+        app.current_rt_graph_index %= 4
+        if frequencies[app.current_rt_graph_index] != "All":
+            app.rt_graph.plot_waveform(frequencies[app.current_rt_graph_index])
         else:
-            rt_graph.plot_all()
-        rt_buttons.update_graph_label(frequencies[current_graph_index])
+            app.rt_graph.plot_all()
+        app.rt_buttons.update_graph_label(frequencies[app.current_rt_graph_index])
 
 
 def update_rt_display():
-    rt_display.setText(f"RT60 Value: {round(abs(rt_graph.rt60), 2)} seconds")
+    app.rt_display.setText(f"RT60 Value: {round(abs(app.rt_graph.rt60), 2)} seconds")
 
 
 def update_max_freq_display():
-    max_freq_display.setText(f"Max Frequency: {round(rt_graph.value_of_max, 2)} Hz")
+    app.max_freq_display.setText(f"Max Frequency: {round(app.rt_graph.value_of_max, 2)} Hz")
 
 
 def file_loaded():  # updates the label of audio duration after the file is loaded
-    global current_graph_index, loaded, spectrogram
-    length_message.setText(f"Audio Duration: {file_load_frame.file.duration} seconds")
-    waveform.plot_waveform()
-    rt_graph.plot_waveform(frequencies[current_graph_index])
-    rt_buttons.update_graph_label(frequencies[current_graph_index])
-    loaded = True
+    app.length_message.setText(f"Audio Duration: {app.file_load_frame.file.duration} seconds")
+    app.waveform.plot_waveform()
+    app.rt_graph.plot_waveform(frequencies[app.current_rt_graph_index])
+    app.rt_buttons.update_graph_label(frequencies[app.current_rt_graph_index])
+    app.is_file_loaded = True
 
 
 def filepath_changed():  # updates file load frame filepath to the filepath in file select frame
-    file_load_frame.filepath = file_select_frame.file_path_label.toPlainText()
+    app.file_load_frame.filepath = app.file_select_frame.file_path_label.toPlainText()
 
 
 def exit_handler():  # runs on exit, deleting the temporary wav file
@@ -55,69 +45,16 @@ def exit_handler():  # runs on exit, deleting the temporary wav file
 
 if __name__ == "__main__":
     # app preparation
-    loaded = False
     atexit.register(exit_handler)
-    app = QApplication(sys.argv)
-    app.setApplicationName("Sound Visualizer")
-    app.setStyleSheet("QWidget { background-color: rgb(54, 48, 98); color: rgb(245, 232, 199); } ")
-    window = QWidget()
-    window.setMinimumSize(1064, 520)
-    layout = QHBoxLayout(window)
-    settings_layout = QVBoxLayout()
-    graph_layout = QVBoxLayout()
+    app = View()
 
-    # graphs setup
-    waveform = wm.WaveformModel()
-    graph_layout.addWidget(waveform)
-    rt_graph = rm.ReverbFormModel()
-    rt_graph.rt60_changed.connect(update_rt_display)
-    rt_graph.max_freq_changed.connect(update_max_freq_display)
-    graph_layout.addWidget(rt_graph)
-    spectrogram = gm.GraphWidget("Spectrogram")
+    # link signals
+    app.rt_graph.rt60_changed.connect(update_rt_display)
+    app.rt_graph.max_freq_changed.connect(update_max_freq_display)
+    app.rt_buttons.next_button.clicked.connect(lambda: set_rt_graph(1))
+    app.rt_buttons.prev_button.clicked.connect(lambda: set_rt_graph(-1))
+    app.file_select_frame.file_path_label.textChanged.connect(filepath_changed)
+    app.file_load_frame.load_signal.connect(file_loaded)
 
-    # graph alternate buttons setup
-    rt_buttons = rtab.AlternateButtonsFrame()
-    rt_buttons.next_button.clicked.connect(lambda: set_rt_graph(1))
-    rt_buttons.prev_button.clicked.connect(lambda: set_rt_graph(-1))
-    graph_layout.addWidget(rt_buttons)
-    current_graph_index = 0
-
-    # file select setup
-    file_select_label = QLabel("Select a sound file to analyze:")
-    file_select_label.setAlignment(Qt.AlignmentFlag.AlignBottom)
-    file_select_label.setMaximumHeight(20)
-    settings_layout.addWidget(file_select_label)
-    file_select_frame = fsf.FileSelectFrame()
-    file_select_frame.setMaximumWidth(500)
-    settings_layout.addWidget(file_select_frame)
-
-    # file length message
-    length_message = QLabel("Audio Duration: ")
-    settings_layout.addWidget(length_message)
-
-    # graph rt display setup
-    rt_display = QLabel("RT60 Value: ")
-    settings_layout.addWidget(rt_display)
-
-    # max freq display setup
-    max_freq_display = QLabel("Maximum Frequency: ")
-    settings_layout.addWidget(max_freq_display)
-
-    # file load setup
-    file_load_frame = flf.FileLoadFrame()
-    file_load_frame.filepath = file_select_frame.file_path_label.toPlainText()
-    file_select_frame.file_path_label.textChanged.connect(filepath_changed)
-    file_load_frame.load_signal.connect(file_loaded)
-    settings_layout.addWidget(file_load_frame)
-
-    # frames setup
-    graph_frame = QFrame()
-    graph_frame.setLayout(graph_layout)
-    settings_frame = QFrame()
-    settings_frame.setLayout(settings_layout)
-    layout.addWidget(settings_frame)
-    layout.addWidget(graph_frame)
-
-    # execution
-    window.show()
+    app.window.show()
     app.exec()
